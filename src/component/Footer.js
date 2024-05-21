3ccdfe9706d55d89e1d3791621373001ba06386d
Dahouda <dahouda37@gmail.com>
2024-05-21 09:30:02 +0200

    first commit

---
+++ src/component/Footer.js
@@ -0,0 +1,17 @@
+
+
+function Footer() {
+  return (
+    
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2024 Copyright:
+        <p>Dr. Dahouda MWAMBA@UPL</p>
+          
+        
+      </div>
+    
+  )
+}
+
+export default Footer
